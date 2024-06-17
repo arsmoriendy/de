12df13c5d85e476dbdfeee071f974cdd4a0f115f
arsmoriendy <nugrahasteven@gmail.com>
2024-06-17 12:34:18 +0700

    docs: usage

--- cli/de/de.go
+++ cli/de/de.go
@@ -9,11 +9,23 @@ import (
 	"github.com/arsmoriendy/de/pkgs/getdefiles"
 )
 
+const usage = `Usage:
+        de <format>
+
+        <format>:
+                Output format
+
+                E.g. "Name: {Name}" would result in "Name: <Desktop Entry Name>"
+`
+
 func main() {
 	home := path.Join(os.Getenv("HOME"), ".local/share/applications/")
 
-	if len(os.Args) != 2 {
-		log.Fatal("Please specify output format")
+	if len(os.Args) < 2 {
+		log.Fatalf(
+			"Please specify output format\n%v",
+			usage,
+		)
 	}
 
 	rstring := getdefiles.GetAllDeIn(
