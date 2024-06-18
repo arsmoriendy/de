efb20dcece0d34c3be403641398c0ac1edc4e2f9
arsmoriendy <nugrahasteven@gmail.com>
2024-06-18 10:18:02 +0700

    fix: removed de usage const

--- cli/de/de.go
+++ cli/de/de.go
@@ -6,15 +6,6 @@ import (
 	"github.com/arsmoriendy/de/pkgs/parsede"
 )
 
-const usage = `Usage:
-        de <format>
-
-        <format>:
-                Output format
-
-                E.g. "Name: {Name}" would result in "Name: <Desktop Entry Name>"
-`
-
 func main() {
 	opts := parseOpts()
 
