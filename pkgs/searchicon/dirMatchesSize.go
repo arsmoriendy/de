0fb5963375493136fd32ef6a20d84518ae6c85ce
arsmoriendy <nugrahasteven@gmail.com>
2024-06-23 16:05:18 +0700

    feat: rewrote dirMatchesSize
    harder, better, faster:
    - harder to read, was hard to write (harder)
    - completed error checking (better)
    - lazy load vars (faster)

--- pkgs/searchicon/dirMatchesSize.go
+++ pkgs/searchicon/dirMatchesSize.go
@@ -1,77 +1,103 @@
 package searchicon
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-func dirMatchesSize(idxFile *os.File, subdir string, iconsize int, iconscale int) bool {
+var dirSizeMismatch = errors.New("theme subdirectory does not match the size constraint specified.")
+var idxFormatErr = errors.New("index.theme file has an invalid format")
+
+// Where idxFile is the index.theme file.
+func dirMatchesSize(idxFile *os.File, subdir string, iconsize int, iconscale int) (bool, error) {
+	// if Scale != iconscale [
+	var scaleint int
 	scalestr, err := getHKV(idxFile, subdir, "Scale")
 	if err != nil {
-		// TODO: handle err
+		// default Scale to 1 as spec if not found
+		scaleint = 1
+	} else {
+		scaleint, err = strconv.Atoi(scalestr)
 	}
-	scaleint, err := strconv.Atoi(scalestr)
+
 	if err != nil {
-		// TODO: handle err
+		return false, fmt.Errorf(
+			`failed to parse "Scale" of %v in file %v: %w: %w`,
+			subdir, idxFile.Name(), err, idxFormatErr)
+	}
+
+	if scaleint != iconscale {
+		return false, nil
+	}
+	// ]
+
+	typestr, err := getHKV(idxFile, subdir, "Type")
+	if err != nil {
+		return false, nil
 	}
 
 	sizestr, err := getHKV(idxFile, subdir, "Size")
 	if err != nil {
-		// TODO: handle err
+		return false, fmt.Errorf(
+			`missing "Size" key of %v in file %v: %w`,
+			subdir, idxFile.Name(), idxFormatErr)
 	}
 	sizeint, err := strconv.Atoi(sizestr)
 	if err != nil {
-		// TODO: handle err
+		return false, fmt.Errorf(
+			`failed to parse "Size" of %v in file %v: %w: %w`,
+			subdir, idxFile.Name(), err, idxFormatErr)
 	}
 
-	minstr, err := getHKV(idxFile, subdir, "MinSize")
-	if err != nil {
-		// TODO: handle err
-	}
-	minint, err := strconv.Atoi(minstr)
-	if err != nil {
-		// TODO: handle err
+	switch typestr {
+	case "Fixed":
+		return sizeint == iconsize, nil
+	case "Scaled":
+		var minint int
+		minstr, err := getHKV(idxFile, subdir, "MinSize")
+		if err != nil {
+			minint = sizeint
+		} else {
+			minint, err = strconv.Atoi(minstr)
+		}
+		if err != nil {
+			return false, fmt.Errorf(
+				`failed to parse "MinSize" of %v in file %v: %w: %w`,
+				subdir, idxFile.Name(), err, idxFormatErr)
+		}
+
+		var maxint int
+		maxstr, err := getHKV(idxFile, subdir, "MaxSize")
+		if err != nil {
+			maxint = sizeint
+		} else {
+			maxint, err = strconv.Atoi(maxstr)
+		}
+		if err != nil {
+			return false, fmt.Errorf(
+				`failed to parse "MaxSize" of %v in file %v: %w: %w`,
+				subdir, idxFile.Name(), err, idxFormatErr)
+		}
+
+		return minint <= iconsize && iconsize <= maxint, nil
+	case "Threshold":
+		var thint int
+		thstr, err := getHKV(idxFile, subdir, "Threshold")
+		if err != nil {
+			thint = 2
+		} else {
+			thint, err = strconv.Atoi(thstr)
+		}
+		if err != nil {
+			return false, fmt.Errorf(
+				`failed to parse "Threshold" of %v in file %v: %w: %w`,
+				subdir, idxFile.Name(), err, idxFormatErr)
+		}
+
+		return sizeint-thint <= iconsize && iconsize <= sizeint+thint, nil
 	}
 
-	maxstr, err := getHKV(idxFile, subdir, "MaxSize")
-	if err != nil {
-		// TODO: handle err
-	}
-	maxint, err := strconv.Atoi(maxstr)
-	if err != nil {
-		// TODO: handle err
-	}
-
-	thstr, err := getHKV(idxFile, subdir, "Threshhold")
-	if err != nil {
-		// TODO: handle err
-	}
-	thint, err := strconv.Atoi(thstr)
-	if err != nil {
-		// TODO: handle err
-	}
-
-	typestr, err := getHKV(idxFile, subdir, "Type")
-	if err != nil {
-		// TODO: handle err
-	}
-
-	// start matching
-	if scaleint != iconscale {
-		return false
-	}
-
-	if typestr == "Fixed" {
-		return sizeint == iconsize
-	}
-
-	if typestr == "Scaled" {
-		return minint <= iconsize && iconsize <= maxint
-	}
-
-	if typestr == "Threshhold" {
-		return sizeint-thint <= iconsize && iconsize <= sizeint+thint
-	}
-
-	return false
+	return false, nil
 }
