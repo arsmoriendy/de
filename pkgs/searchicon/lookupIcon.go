c52881fc57d8a58226405dee36412aaf2780d165
arsmoriendy <nugrahasteven@gmail.com>
2024-07-12 17:15:52 +0700

    feat: lookupIcon first outer loop

--- pkgs/searchicon/lookupIcon.go
+++ pkgs/searchicon/lookupIcon.go
@@ -1,21 +1,61 @@
 package searchicon
 
-func lookupIcon(iconname string, size string, scale string, theme string) string {
+import (
+	"io/fs"
+	"os"
+	"path"
+)
+
+// iconname does not include the file extension (e.g. "icon" not "icon.svg")
+func lookupIcon(iconname string, size int, scale int, theme string) (string, error) {
 	rstring := ""
 
 	idxFile, err := getThemeIdx(getThemeDirs(theme))
 	if err != nil {
-		// TODO: handle err
+		return "", err
 	}
 
 	subdirs, err := getHKV(idxFile.Name(), "Icon Theme", "Directories")
 	if err != nil {
-		// TODO: handle err
+		return "", err
 	}
 
-	walkDirsStr(subdirs, ',', func(dir string) error {
-		return nil
+	// Append directories listed in ScaledDirectories if any for backwards compatibility
+	scaledSubdirs, err := getHKV(idxFile.Name(), "Icon Theme", "ScaledDirectories")
+	if err == nil {
+		subdirs += "," + scaledSubdirs
+	}
+
+	walkDirsStr(subdirs, ',', func(subdir string) error {
+		var wds1rerr error = nil
+
+		matches, _ := dirMatchesSize(idxFile, subdir, size, scale)
+		if !matches {
+			return nil
+		}
+
+		walkDirsStr(baseDirs, ':', func(directory string) error {
+			dirpath := path.Join(directory, theme, subdir)
+
+			for _, ext := range []string{"png", "svg", "xpm"} {
+				filename := path.Join(dirpath, iconname+"."+ext)
+
+				if _, err = os.Stat(filename); err != nil {
+					continue
+				}
+
+				rstring = filename
+				wds1rerr = fs.SkipAll
+				return fs.SkipAll
+			}
+
+			return nil
+		})
+
+		return wds1rerr
 	})
 
-	return rstring
+	// TODO: DirectorySizeDistance loop
+
+	return rstring, nil
 }
