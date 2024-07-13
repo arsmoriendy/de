f40f2119807445b9d3eb30491b57bac5b694ff9d
arsmoriendy <nugrahasteven@gmail.com>
2024-07-13 16:10:44 +0700

    feat: dirSizeDistance threshold

--- pkgs/searchicon/dirSizeDistance.go
+++ pkgs/searchicon/dirSizeDistance.go
@@ -93,6 +93,81 @@ func dirSizeDistance(idxFile *os.File, subdir string, iconsize int, iconscale in
 			return iconsize*iconscale - maxsizeint*scaleint, nil
 		}
 		return 0, nil
+
+	case "Threshold":
+		// get Size [
+		sizestr, err := getHKV(idxFile.Name(), subdir, "Size")
+		if err != nil {
+			err = fmt.Errorf("%w: %w", dirSizeDistanceErr, err)
+			return 0, err
+		}
+
+		sizeint, err := strconv.Atoi(sizestr)
+		if err != nil {
+			err = fmt.Errorf("%w: %w", dirSizeDistanceErr, err)
+			return 0, err
+		}
+		// ]
+
+		// get Threshold {
+		thresholdstr, err := getHKV(idxFile.Name(), subdir, "Threshold")
+		if err != nil {
+			err = fmt.Errorf("%w: %w: %w", dirSizeDistanceErr,
+				errors.New("cannot find Threshold value"),
+				err)
+			break
+		}
+
+		thresholdint, err := strconv.Atoi(thresholdstr)
+		if err != nil {
+			err = fmt.Errorf("%w: %w: %w", dirSizeDistanceErr,
+				errors.New("failed to convert Threshold to int"),
+				err)
+			return 0, err
+		}
+		// }
+
+		// get MinSize [
+		var minsizeint int
+
+		minsizestr, err := getHKV(idxFile.Name(), subdir, "MinSize")
+		if err != nil {
+			break
+		}
+
+		minsizeint, err = strconv.Atoi(minsizestr)
+		if err != nil {
+			err = fmt.Errorf("%w: %w: %w", dirSizeDistanceErr,
+				errors.New("failed to convert MinSize to int"),
+				err)
+			return 0, err
+		}
+		// ]
+
+		// get MaxSize [
+		var maxsizeint int
+
+		maxsizestr, err := getHKV(idxFile.Name(), subdir, "MaxSize")
+		if err != nil {
+			break
+		}
+
+		maxsizeint, err = strconv.Atoi(maxsizestr)
+		if err != nil {
+			err = fmt.Errorf("%w: %w: %w", dirSizeDistanceErr,
+				errors.New("failed to convert MaxSize to int"),
+				err)
+			return 0, err
+		}
+		// ]
+
+		if iconsize*iconscale < (sizeint-thresholdint)*scaleint {
+			return minsizeint*scaleint - iconsize*iconscale, nil
+		}
+		if iconsize*iconscale > (sizeint+thresholdint)*scaleint {
+			return iconscale*iconsize - maxsizeint*scaleint, nil
+		}
+		return 0, nil
 	}
 
 	return 0, nil
