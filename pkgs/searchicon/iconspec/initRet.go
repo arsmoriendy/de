a78712e35b412c2221355dfa19b2f7a7a4552c8f
arsmoriendy <nugrahasteven@gmail.com>
2024-07-14 15:02:23 +0700

    feat: initRet for ints

--- pkgs/searchicon/iconspec/initRet.go
+++ pkgs/searchicon/iconspec/initRet.go
@@ -3,7 +3,7 @@ package iconspec
 // If init is true, return the value of *a.
 //
 // If init is false, initializes *a to the value of initv(). Returns the value of *a.
-func initRet[T string](a *T, init bool, initv func() (T, error)) (T, error) {
+func initRet[T string | int](a *T, init bool, initv func() (T, error)) (T, error) {
 	if init {
 		return *a, nil
 	}
