b190c395253870638c43fbfb6e8eccf2991473a3
arsmoriendy <nugrahasteven@gmail.com>
2024-07-27 14:09:17 +0700

    refactor: use thelper on dirspec test

--- pkgs/searchicon/iconspec/DirSpec_test.go
+++ pkgs/searchicon/iconspec/DirSpec_test.go
@@ -1,11 +1,11 @@
 package iconspec_test
 
 import (
-	"fmt"
 	"path/filepath"
 	"testing"
 
 	"github.com/arsmoriendy/de/pkgs/searchicon/iconspec"
+	th "github.com/arsmoriendy/de/internal/thelper"
 )
 
 func TestDirSpec(t *testing.T) {
@@ -15,50 +15,23 @@ func TestDirSpec(t *testing.T) {
 	ds := iconspec.NewDir(idxFn, subdir)
 
 	// Size
-	fataliferr(t, okasexp(ds.Size, 128))
+	th.FatalIfErr(t, th.OkAsExp(ds.Size, 128))
 
 	// Scale
-	fataliferr(t, asexp(ds.Scale(), 1))
+	th.FatalIfErr(t, th.AsExp(ds.Scale(), 1))
 
 	// Context
-	fataliferr(t, okasexp(ds.Context, "Devices"))
+	th.FatalIfErr(t, th.OkAsExp(ds.Context, "Devices"))
 
 	// Type
-	fataliferr(t, okasexp(ds.Type, "Scalable"))
+	th.FatalIfErr(t, th.OkAsExp(ds.Type, "Scalable"))
 
 	// MaxSize
-	fataliferr(t, okasexp(ds.MaxSize, 512))
+	th.FatalIfErr(t, th.OkAsExp(ds.MaxSize, 512))
 
 	// MinSize
-	fataliferr(t, okasexp(ds.MinSize, 8))
+	th.FatalIfErr(t, th.OkAsExp(ds.MinSize, 8))
 
 	// Threshold
-	fataliferr(t, asexp(ds.Threshold(), 2))
-}
-
-func fataliferr(t *testing.T, err error) {
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-// *As expected*. Checks if `in` is the same as `exp`.
-func asexp[T comparable](in T, exp T) error {
-	if in != exp {
-		return fmt.Errorf("\nExpected:\t%v\nGot Result:\t%v", exp, in)
-	}
-
-	return nil
-}
-
-// *Ok(no errors) and as expected*.
-// Wrapper for handling `f`'s error and running asexp on it's return value.
-func okasexp[T comparable](f func() (T, error), exp T) error {
-	in, err := f()
-
-	if err != nil {
-		return err
-	}
-
-	return asexp(in, exp)
+	th.FatalIfErr(t, th.AsExp(ds.Threshold(), 2))
 }
